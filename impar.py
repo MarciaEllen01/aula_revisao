c78cff6398ebdfc10a97c3c0167b94fea8a36f07
Márcia Ellen <ellenmarica6@gmail.com>
2025-07-04 14:39:36 +0000

    testes da aula

---
+++ impar.py
@@ -0,0 +1,5 @@
+impar = 0 
+while impar <= 20:
+    if impar % 2 != 0:
+        print(impar)
+    impar += 1 
--- orojeto.py
+++ orojeto.py
@@ -1,1 +1,5 @@
-print("Oi mundo")
+par = 0 
+while par <= 20:
+    if par % 2 == 0:
+        print(par)
+    par += 1 
